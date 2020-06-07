1324560523beb0df5aae35231fdf3f819c58838f
Nikita Zhidunov <zhidunov@gmail.com>
2020-06-08 02:22:55 +0300

    Updates

---
+++ precache-manifest.684b446c538fccb678b87fcf227fad85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340db8e6e2faf8f7f25b33279462c49d",
+    "url": "/RegLogForm/index.html"
+  },
+  {
+    "revision": "7d134da256c3cba3ab57",
+    "url": "/RegLogForm/static/css/main.9955f554.chunk.css"
+  },
+  {
+    "revision": "395416068eb092f6fd5a",
+    "url": "/RegLogForm/static/js/2.67855730.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RegLogForm/static/js/2.67855730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d134da256c3cba3ab57",
+    "url": "/RegLogForm/static/js/main.b9710046.chunk.js"
+  },
+  {
+    "revision": "095c3bd5b1e9a4ed5fc2",
+    "url": "/RegLogForm/static/js/runtime-main.276871b9.js"
+  },
+  {
+    "revision": "111c8fbf1db66a58b4503a57c37e49b0",
+    "url": "/RegLogForm/static/media/SF-Compact-Display-Regular.111c8fbf.svg"
+  },
+  {
+    "revision": "4c54cb14c8c4b7462e0b86757c83d201",
+    "url": "/RegLogForm/static/media/SF-Compact-Display-Regular.4c54cb14.eot"
+  },
+  {
+    "revision": "542faf8e2b08ea3205f21d4a7b5f28eb",
+    "url": "/RegLogForm/static/media/SF-Compact-Display-Regular.542faf8e.ttf"
+  },
+  {
+    "revision": "5edd8bf1e97c866cba122ce8bb159881",
+    "url": "/RegLogForm/static/media/SF-Compact-Display-Regular.5edd8bf1.woff"
+  },
+  {
+    "revision": "bdb6eb66a2a7e6b914752cad80eb2ccb",
+    "url": "/RegLogForm/static/media/eye.bdb6eb66.svg"
+  }
+]);
